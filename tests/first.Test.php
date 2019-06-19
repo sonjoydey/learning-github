5fcf656706c99513dda62b0964e3d4ab6d2de80e
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-19 16:15:23 -0400

    Test case made

--- tests/first.Test.php
+++ tests/first.Test.php
@@ -3,8 +3,20 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testTrue()
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
+    }
+
+    public function testFalse()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testMultiple()
+    {
+        $this->assertEquals(5, 2+3);
+        $this->assertEquals(5, 2+2);
+        
     }
 }
