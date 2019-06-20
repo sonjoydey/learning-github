d611406832413a1a97bfa1f5a195e14901aa65ce
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-20 09:57:48 -0400

    Success Test

--- tests/first.Test.php
+++ tests/first.Test.php
@@ -12,21 +12,21 @@ class StackTest extends TestCase
 
     public function testexpectedStringAnother()
     {
-        $this->assertTrue(expectedString("failone"));
-        $this->assertFalse(expectedString("jenkins"));
+        $this->assertTrue(expectedString("jenkins"));
+        $this->assertFalse(expectedString("false"));
     }
 
     public function testMultiple()
     {
         $this->assertEquals(5, add(3, 2));
-        $this->assertEquals(4, add(3, 2));
+        $this->assertEquals(6, add(3, 3));
     }
 
     public function testMultipleAgain()
     {
         $this->assertEquals(5, add(3, 2));
-        $this->assertEquals(4, add(3, 2));
-        $this->assertEquals(6, add(3, 2));
-        $this->assertEquals(7, add(3, 2));
+        $this->assertEquals(4, add(2, 2));
+        $this->assertEquals(6, add(4, 2));
+        $this->assertEquals(7, add(3, 4));
     }
 }
