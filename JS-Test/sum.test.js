3011b01a3a60c001343a69e0a2c6ced45abf4425
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-21 15:09:13 -0400

    Fail Test

--- JS-Test/sum.test.js
+++ JS-Test/sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
+test('Result for 1+2', () => {
   expect(sum(1, 2)).toBe(4);
+});
+
+test('Result for 5+2', () => {
+  expect(sum(5, 2)).toBe(7);
+});
+
+test('Result for 3+2', () => {
+  expect(sum(3, 2)).toBe(7);
 });
