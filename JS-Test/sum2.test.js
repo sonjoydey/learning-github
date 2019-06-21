5292f1ccb9c11f6e2ea56b6240f7dada587279fc
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-21 16:34:10 -0400

    Success Test

--- JS-Test/sum2.test.js
+++ JS-Test/sum2.test.js
@@ -1,5 +1,9 @@
 const sub = require('./sum2');
 
-test('Result for 28-2', () => {
-  expect(sub(28, 2)).toBe(26);
+describe('File -1', () => {
+	
+	test('Result for 28-2', () => {
+	  expect(sub(28, 2)).toBe(26);
+	});
+
 });
