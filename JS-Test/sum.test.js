f3bbf1d618165e645e86f9c3eee348504c4a5c0b
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-21 16:32:59 -0400

    Success Test

--- JS-Test/sum.test.js
+++ JS-Test/sum.test.js
@@ -1,23 +1,22 @@
 const sum = require('./sum');
 const sub = require('./sum2');
 
-test.todo('Group All'){
-	expect(sum(1, 2)).toBe(3);
-	expect(sum(5, 2)).toBe(7);
-};
+describe('matching cities to foods', () => {
 
-test('Result for 1+2', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+	test('Result for 1+2', () => {
+	  expect(sum(1, 2)).toBe(3);
+	});
 
-test('Result for 5+2', () => {
-  expect(sum(5, 2)).toBe(7);
-});
+	test('Result for 5+2', () => {
+	  expect(sum(5, 2)).toBe(7);
+	});
 
-test('Result for 3+2', () => {
-  expect(sum(3, 2)).toBe(5);
-});
+	test('Result for 3+2', () => {
+	  expect(sum(3, 2)).toBe(5);
+	});
 
-test('Result for 25-2', () => {
-  expect(sub(25, 2)).toBe(23);
+	test('Result for 25-2', () => {
+	  expect(sub(25, 2)).toBe(23);
+	});
+
 });
