670d323d3625e05a55b8a2e642bca8f890c5d97d
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-21 16:22:32 -0400

    Success Test

--- JS-Test/sum.test.js
+++ JS-Test/sum.test.js
@@ -1,21 +1,20 @@
 const sum = require('./sum');
 const sub = require('./sum2');
 
-test('File1', () => {
+test.todo('Group All');
 
-	test('Result for 1+2', () => {
-	  expect(sum(1, 2)).toBe(3);
-	});
+test('Result for 1+2', () => {
+  expect(sum(1, 2)).toBe(3);
+});
 
-	test('Result for 5+2', () => {
-	  expect(sum(5, 2)).toBe(7);
-	});
+test('Result for 5+2', () => {
+  expect(sum(5, 2)).toBe(7);
+});
 
-	test('Result for 3+2', () => {
-	  expect(sum(3, 2)).toBe(5);
-	});
+test('Result for 3+2', () => {
+  expect(sum(3, 2)).toBe(5);
+});
 
-	test('Result for 25-2', () => {
-	  expect(sub(25, 2)).toBe(23);
-	});
+test('Result for 25-2', () => {
+  expect(sub(25, 2)).toBe(23);
 });
