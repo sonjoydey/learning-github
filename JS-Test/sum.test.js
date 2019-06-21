744c72079f7af9a58a66c2419d4ffb4789435643
Sonjoy Dey <sonjoydey07@gmail.com>
2019-06-21 16:45:37 -0400

    Success Test

--- JS-Test/sum.test.js
+++ JS-Test/sum.test.js
@@ -20,3 +20,18 @@ describe('File -1', () => {
 	});
 
 });
+
+const myBeverage = {
+  delicious: true,
+  sour: false,
+};
+
+describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(myBeverage.delicious).toBeTruthy();
+  });
+
+  test('is not sour', () => {
+    expect(myBeverage.sour).toBeFalsy();
+  });
+});
